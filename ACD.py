b80be3fa55e55b23585651ed66865c71df2d2703
Dennis Sell <sell.dennis@gmail.com>
2015-02-16 20:55:16 -0500

    Expanded to arbitrary k, t

--- ACD.py
+++ ACD.py
@@ -11,8 +11,8 @@ from operator import mul
 p = 131
 q = 41
 q1 = 37
-r1 = 10
-X1 = 12
+r1 = 5
+X1 = 8
 
 N = p * q
 a1 = q1 * p + r1
@@ -25,24 +25,56 @@ def satisfies_conditions(N, beta, bounds):
     return bool(beta_bound) and bool(noise_bound)
 
 
-def howgrave_graham(N, beta, a1, X1):
+def simple_howgrave_graham(N, a1, X1):
     L = matrix(ZZ, 2, 2,
-                  [ -1*X1, a1,
-                        0,  N])
+                  [[ -1*X1, a1],
+                   [    0,  N]])
     B = L.LLL()
-    print B
 
     x = PolynomialRing(RationalField(), 'x').gen()
     f = sum(B[0][1-i] * x**i / float(X1)**i for i in range(2))
     m = f.roots()
-    print m
-
     # How to get to a value of r?
     return m[0][0]
 
 
+def make_hg_rows(N, a, x, k, t):
+    L = []
+    for i in range(t):
+        current_row = []
+
+        for j in range(t):
+            coefficient = binomial(i, j) * pow(-1 * a1, i-j) * pow(X1, j)
+            coefficient *= pow(N, k-i) if i <= k else 1 # might be < k
+            current_row.insert(0, coefficient)
+        L.append(current_row)
+    return L
+
+
+def howgrave_graham_formula(k, t):
+    var("a,X,N")
+    L = make_hg_rows(N, a, X, k, t)
+    L = matrix(t, t, L)
+    return L
+
+
+def howgrave_graham(N, a1, X1, k, t):
+    print "Howgrave graham for k=%i, t=%i" % (k,t)
+    L = make_hg_rows(N, a1, X1, k, t)
+    L = matrix(ZZ, t, t, L)
+    print L
+
+    B = L.LLL()
+    print B
+    x = PolynomialRing(RationalField(), 'x').gen()
+    f = sum(B[0][1-i] * x**i / float(X1)**i for i in range(2))
+    return f.roots()
+
 
 print satisfies_conditions(N, log(p, N), [X1])
-r = howgrave_graham(N, log(p, N), a1, X1)
-print r - a1
+r = simple_howgrave_graham(N, a1, X1)
+
+
+#print howgrave_graham_formula(2, 3)
+print howgrave_graham(N, a1, X1, 2, 3)
 
